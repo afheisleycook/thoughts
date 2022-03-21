23e56444bd2ea71b4cd38dc744338b927af0dd15
austin <ad>
2022-03-20 20:04:09 -0700

    added

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -8,8 +8,13 @@ name austin heislev-cook
 
 var db = require("mongodb");
 
-app = app.express();
+app = express();
 app.all('/', function (req, res) {
   res.send("hello");
+});
+app.post('/post', function (req, res) {
+  var title = req.get("title");
+  console.log(title);
+  res.send(title.toString());
 });
 app.listen(8000);
--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const express = require("express");
 name austin heislev-cook
 */
 const db = require("mongodb");
-app = app.express();
+app = express();
 app.all('/', function(req, res) {
     res.send("hello");
 
+});
+app.post('/post', function(req, res) {
+    var title = req.get("title");
+    console.log(title);
+    res.send(title.toString());
 });
 
 app.listen(8000);
