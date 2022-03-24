73fd195583b1c4b54ec19b663950931b5a9843d7
austin <ad>
2022-03-24 12:07:47 -0700

    hello

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ app.use(express.static('public'));
 app.post('/add', function(req, res) {
     var data = req.body;
     console.log(data);
-    db = sqlite3.Database("thoughts.db");
-    db.run("insert into THOUGHTS values({data[0]},{data[1]},{data[2]})").then
+    db = new sqlite3.Database("thoughts.db");
+    db.run("insert into THOUGHTS values(0,{data[0]},{data[1]},{data[2]})").then
     console.log("inserted ", data);
 });
 app.listen(3010);
